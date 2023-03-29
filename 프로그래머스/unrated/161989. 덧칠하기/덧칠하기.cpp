66849b22b29cfc99e9d9372749d7ed07c689048a
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-03-29 22:29:47 +0900

    [unrated] Title: 덧칠하기, Time: 0.22 ms, Memory: 5.04 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int cur = 0;
+    int answer = 0;
+    for(int i=0; i<section.size(); i++){
+        if(cur > section[i]) continue;
+        answer++;
+        cur = section[i] + m;
+    }
+    return answer;
+}
