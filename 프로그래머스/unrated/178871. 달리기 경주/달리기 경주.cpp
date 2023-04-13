4dd0009f240069ea50cc03f05d4720215f07eab0
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-04-13 12:29:18 +0900

    [unrated] Title: 달리기 경주, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+vector<string> solution(vector<string> players, vector<string> callings) {
+    map<int, string> m1;
+    map<string, int> m2;
+    
+    for(int i=0; i<players.size(); i++) {
+        m1[i] = players[i];
+        m2[players[i]] = i;
+    }
+    
+    for(int i=0; i<callings.size(); i++) {
+        int cur_idx = m2[callings[i]];
+        string change = m1[cur_idx - 1];
+        
+        m1[cur_idx] = change;
+        m1[cur_idx - 1] = callings[i];
+        m2[change] = cur_idx;
+        m2[callings[i]] = cur_idx - 1;
+    }
+    
+    vector<string> ans;
+    
+    for(auto c : m1)
+        ans.push_back(c.second);
+    return ans;
+}
