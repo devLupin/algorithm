2d98491e464d3674ea52899c293b9244dc1194ec
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-04-17 21:34:56 +0900

    Update 숫자 변환하기.cpp

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -19,16 +19,16 @@ int bfs(int x, int y, int n) {
 
         if (cx == y)
             return cur.second;
-        
-        if (cx * 2 < INF && !visited[cx * 2]) {
+
+        if (cx * 2 <= y && !visited[cx * 2]) {
             q.push({ cx * 2, cur.second + 1 });
             visited[cx * 2] = true;
         }
-        if (cx * 3 < INF && !visited[cx * 3]) {
+        if (cx * 3 <= y && !visited[cx * 3]) {
             q.push({ cx * 3, cur.second + 1 });
             visited[cx * 3] = true;
         }
-        if (cx + n < INF && cx + n > 0 && !visited[cx + n]) {
+        if (cx + n <= y && !visited[cx + n]) {
             q.push({ cx + n, cur.second + 1 });
             visited[cx + n] = true;
         }
@@ -37,6 +37,21 @@ int bfs(int x, int y, int n) {
     return -1;
 }
 
+int memo[INF];
+int DP(int x, int y, int n) {
+    fill_n(memo, INF, INF);
+    memo[x] = 0;
+
+    for (int i = x; i <= y; i++) {
+        if (i * 2 <= y) memo[i * 2] = min(memo[i * 2], memo[i] + 1);
+        if (i * 3 <= y) memo[i * 3] = min(memo[i * 3], memo[i] + 1);
+        if (i + n <= y) memo[i + n] = min(memo[i + n], memo[i] + 1);
+    }
+
+    return (memo[y] == INF) ? -1 : memo[y];
+}
+
 int solution(int x, int y, int n) {
+    return DP(x, y, n);
     return bfs(x, y, n);
 }
