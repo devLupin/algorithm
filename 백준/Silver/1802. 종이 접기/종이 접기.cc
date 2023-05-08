048d7b004b4cd3c90f3d34df8d0060236d8fa553
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-05-08 11:00:11 +0900

    [Silver I] Title: 종이 접기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/1802. 종이 접기/종이 접기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+bool search(string in, int start, int end)
+{
+	if (start >= end)
+		return true;
+
+	int left = start, right = end;
+
+	while (left < right)
+		if (in[left++] == in[right--])
+			return false;
+
+	return search(in, start, right - 1);
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int tc; cin >> tc;
+	while (tc--) {
+		string in;
+		cin >> in;
+
+		if (in.size() % 2 == 0) {
+			cout << "NO\n";
+			continue;
+		}
+
+		bool ans = search(in, 0, in.size() - 1);
+		cout << ((ans) ? "YES\n" : "NO\n");
+	}
+
+	return 0;
+}
