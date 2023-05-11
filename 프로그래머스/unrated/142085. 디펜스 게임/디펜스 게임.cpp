2017d07c432d5daab9d4eff65f29a67df3d15688
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-05-11 18:10:20 +0900

    [unrated] Title: 디펜스 게임, Time: 0.02 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int n, int k, vector<int> enemy) {
+    int sum=0;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    for(int i=0; i<enemy.size(); i++) {
+        pq.push(enemy[i]);
+        
+        if(pq.size() > k){
+            sum += pq.top();
+            pq.pop();
+        }
+        
+        if(sum > n)
+            return i;
+    }
+}
