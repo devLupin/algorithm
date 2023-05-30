ccab8da885c43aed6211f16da3be251f05eefc0c
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-05-31 01:39:41 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,4 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<iostream>
 int n,a[]={1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596,2279184};
-int main(){cin >> n;cout<<a[n-1];}
+int main(){std::cin >> n;std::cout<<a[n-1];}
