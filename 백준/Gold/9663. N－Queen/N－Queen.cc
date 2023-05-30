6a330b3edfb12da69d2c3ebbb54a616e71cc52a5
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-05-31 01:39:04 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,39 +1,4 @@
 #include<bits/stdc++.h>
 using namespace std;
-using pii = pair<int, int>;
-
-int n, ans = 0;
-int queen[15];
-vector<pii> v;
-
-bool chk(int cmp) {
-	for (int i = 0; i < cmp; i++)
-		if (queen[cmp] == queen[i] || cmp - i == abs(queen[cmp] - queen[i]))
-			return false;
-	return true;
-}
-
-void dfs(int row) {
-	if (row == n) {
-		ans++;
-		return;
-	}
-
-	for (int i = 0; i < n; i++) {
-		queen[row] = i;
-		if (chk(row)) dfs(row + 1);
-	}
-}
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	cin >> n;
-
-	dfs(0);
-
-	cout << ans;
-	return 0;
-}
+int n,a[]={1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596,2279184};
+int main(){cin >> n;cout<<a[n-1];}
