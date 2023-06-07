c261bffdbd1a92a7faae2ce69957fc2f47ebe275
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-06-08 01:40:49 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << a + b << '\n';
+	cout << a - b << '\n';
+	cout << a * b << '\n';
+	cout << a / b << '\n';
+	cout << a % b << '\n';
+
+	return 0;
+}
