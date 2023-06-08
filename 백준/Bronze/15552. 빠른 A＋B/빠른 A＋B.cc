1c9b3f9f763c2830c18236d86c3fc62898470488
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-06-08 17:16:24 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 148 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int TC; cin >> TC;
+	while (TC--) {
+		int a, b;
+		cin >> a >> b;
+		cout << a + b << '\n';
+	}
+}
