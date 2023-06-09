d766c689e0b9d26f89a907932a16664555e97d7a
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-06-09 12:08:01 +0900

    [Silver IV] Title: 큐 2, Time: 428 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	queue<int> q;
+
+	int N; cin >> N;
+	while (N--) {
+		string opt; cin >> opt;
+
+		if (opt == "push") {
+			int tmp; cin >> tmp;
+			q.push(tmp);
+		}
+		else if (opt == "pop") {
+			if (q.empty()) cout << -1 << '\n';
+			else {
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (opt == "size")
+			cout << q.size() << '\n';
+		else if (opt == "empty")
+			cout << ((q.empty()) ? 1 : 0) << '\n';
+		else if (opt == "front")
+			cout << (!q.empty() ? q.front() : -1) << '\n';
+		else
+			cout << (!q.empty() ? q.back() : -1) << '\n';
+	}
+
+	return 0;
+}
