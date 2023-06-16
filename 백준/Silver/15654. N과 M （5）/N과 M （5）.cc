537942cbecc3d1686b8fb7078da2a414c2afeabc
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-06-16 18:55:26 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 2108 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int SZ = 10001;
+int n, m, arr[SZ], v[SZ];
+bool vis[SZ];
+
+void solve(int cnt, int s) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) cout << v[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (vis[i]) continue;
+
+		v[cnt] = arr[i];
+		vis[i] = true;
+		solve(cnt + 1, i);
+		v[cnt] = 0;
+		vis[i] = false;
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+	
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	sort(arr, arr + n);
+	
+	solve(0, -1);
+
+	return 0;
+}
