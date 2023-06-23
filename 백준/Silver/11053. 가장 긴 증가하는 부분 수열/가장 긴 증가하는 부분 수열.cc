08467b3a6c69d5c5d53e74cbc5eb5206e41fc812
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-06-24 08:23:59 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,28 +1,26 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int DP[1000];
-int arr[1000];
+const int SZ = 1002;
+int n, arr[SZ], DP[SZ];
 
-int main(void) {
+int main(void)
+{
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(NULL);
 
-	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++)
+	for(int i=0; i<n; i++)
 		cin >> arr[i];
-
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
+	
+	for(int i=0; i<n; i++) {
 		DP[i] = 1;
-		for (int j = 0; j < i; j++) {
-			if (arr[j] < arr[i] && DP[i] < DP[j] + 1)
-				DP[i] = DP[j] + 1;
+		for(int j=0; j<i; j++) {
+			if(arr[i] > arr[j])
+				DP[i] = max(DP[i], DP[j] + 1);
 		}
-		ans = (ans > DP[i]) ? ans : DP[i];
 	}
 
-	cout << ans;
-	return 0;
+	cout << *max_element(DP, DP+n);
+    return 0;
 }
