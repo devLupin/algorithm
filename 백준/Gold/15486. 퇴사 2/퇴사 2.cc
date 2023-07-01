403a4051afc03eb12dbafe6739f05b7274cad96d
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-07-01 09:52:54 +0900

    [Gold V] Title: 퇴사 2, Time: 212 ms, Memory: 13740 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 const int SZ = 1500005;
-int n, T[SZ], P[SZ], DP[SZ];
+int n, T[SZ], P[SZ];
 
 int main(void)
 {
@@ -15,10 +15,10 @@ int main(void)
 	}
 
 	for(int i=n; i>=1; i--) {
-		if(i + T[i] <= n+1) DP[i] = max(DP[i+T[i]] + P[i], DP[i+1]);
-		else DP[i] = DP[i+1];
+		if(i + T[i] <= n+1) P[i] = max(P[i] + P[i + T[i]], P[i+1]);
+		else P[i] = P[i+1];
 	}
 
-	cout << *max_element(DP, DP+n+1);
+	cout << *max_element(P, P+n+1);
 	return 0;
 }
