26b03982225d77e09471d6049ddf473042d044dc
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-07-06 02:28:53 +0900

    [level 2] Title: 올바른 괄호, Time: 0.54 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solution(string s)
+{
+    stack<char> st;
+    
+    for(char c : s) {
+        if(c == '(') st.push(c);
+        else {
+            if(st.empty()) return false;
+            st.pop();
+        }
+    }
+    
+    return st.empty();
+}
