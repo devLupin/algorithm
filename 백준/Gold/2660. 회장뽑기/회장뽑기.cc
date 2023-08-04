9799313530d7402f8effca797a1d65a1f95eed27
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-08-04 09:34:31 +0900

    [Gold V] Title: 회장뽑기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int,int>;
+
+const int SZ = 55;
+bool adj[SZ][SZ], vis[SZ];
+vector<int> pres[50];
+int n, a, b;
+
+int bfs(int s) {
+    int ret = 0;
+    queue<pii> q;
+    q.push({s, 0});
+    vis[s] = true;
+
+    while(!q.empty()) {
+        pii cur = q.front();
+        q.pop();
+
+        ret = max(ret, cur.second);
+
+        for(int i=1; i<=n; i++) {
+            if(vis[i] || !adj[cur.first][i]) continue;
+            vis[i] = true;
+            q.push({i, cur.second+1});
+        }
+    }
+
+    return ret;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    while (true) {
+        cin >> a >> b;
+        if (a == -1 && b == -1) break;
+        adj[a][b] = adj[b][a] = true;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        fill_n(&vis[0], SZ, false);
+        int cur = bfs(i);
+        pres[cur].push_back(i);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (pres[i].empty()) continue;
+
+        sort(pres[i].begin(), pres[i].end());
+
+        cout << i << ' ' << pres[i].size() << '\n';
+        for (int x : pres[i]) cout << x << ' ';
+
+        break;
+    }
+
+    return 0;
+}
