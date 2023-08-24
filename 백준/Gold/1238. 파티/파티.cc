bfc89ba45e77e0d954410907dd33478cb09f0c32
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-08-24 11:46:08 +0900

    [Gold III] Title: 파티, Time: 212 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int,int>;
+
+const int SZ = 1002, INF = 0x3f3f3f3f;
+int n, m, dest, ans;
+vector<pii> adj[SZ];
+bool vis[SZ];
+
+
+int solve(int src, int dst) {
+	int D[SZ];
+	fill(D+1, D+n+1, INF);
+
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	pq.push({0, src});
+	D[src] = 0;
+
+	for(int u, v, w, dw; !pq.empty();) {
+		tie(w, u) = pq.top();
+		pq.pop();
+
+		for(auto nxt : adj[u]) {
+			tie(dw, v) = nxt;
+
+			if(dw + w < D[v]) {
+				D[v] = dw + w;
+				pq.push({dw+w, v});
+			}
+		}
+	}
+
+	return D[dst];
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m >> dest;
+	for(int u, v, c; m--;) {
+		cin >> u >> v >> c;
+		adj[u].push_back({c, v});
+	}
+
+	int ans = 0;
+	for(int i=1; i<=n; i++)
+		ans = max(ans, solve(i, dest) + solve(dest, i));
+	cout << ans;
+
+	return 0;
+}
