7fb0be2a3ec64f504261a920fbe693cd3034a729
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-09-22 15:11:27 +0900

    [Silver III] Title: 서강근육맨, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ull = unsigned long long;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	ull m = 0;
+	int n;
+	cin >> n;
+
+	vector<ull> v(n);
+	for(int i=0; i<n; i++)
+		cin >> v[i];
+
+	sort(v.begin(), v.end());
+
+	if(v.size() % 2 == 0) {
+		int i=0, j=v.size()-1;
+		while(i < j) {
+			m = max(v[i] + v[j], m);
+			i++, j--;
+		}
+	}
+	else {
+		int i=0, j=v.size()-2;
+		while(i < j) {
+			m = max(v[i] + v[j], m);
+			i++, j--;
+		}
+	}
+
+	cout << m;
+}
