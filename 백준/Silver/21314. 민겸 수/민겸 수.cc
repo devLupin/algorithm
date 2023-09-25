e7357ad544205233ad5f6cf116d39241e1582b81
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-09-26 01:16:40 +0900

    [Silver II] Title: 민겸 수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/21314. 민겸 수/민겸 수.cc
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+string str;
+
+string find_max(){
+    stack<char> st;
+    int M = 0;
+    int K = 0;
+    int ans = 0;
+    string temp = "";
+    for(int i = 0;i<str.size();i++){
+        if(st.empty()){
+            if(str[i] == 'K'){
+                temp.append(to_string(5));
+            }
+            else if(str[i] == 'M'){
+                st.push(str[i]);
+            }
+        }
+        else{
+            if(st.top() == 'M' && str[i] == 'K'){
+                temp.append(to_string(5));
+                for(int i = 0;i<st.size();i++){
+                    temp.append(to_string(0));
+                }
+                while(!st.empty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(str[i]);
+            }
+        }
+    }
+
+       if(!st.empty()){
+           for(int i = 0;i<st.size();i++){
+               temp.append(to_string(1));
+           }
+       }
+
+
+    return temp;
+}
+
+string find_min(){
+    vector<char> M;
+    string temp = "";
+    for(int i = 0;i<str.size();i++){
+        if(str[i] == 'K'){
+            if(!M.empty()){
+                temp.append(to_string(1));
+                for(int i = 0;i<M.size()-1;i++){
+                    temp.append(to_string(0));
+                }
+                temp.append(to_string(5));
+                M.clear();
+            }
+            else if(M.empty()){
+                temp.append(to_string(5));
+            }
+        }
+        else if(str[i] == 'M'){
+            M.push_back('M');
+        }
+    }
+
+    if(!M.empty()){
+        temp.append(to_string(1));
+        for(int i = 0;i<M.size()-1;i++){
+            temp.append(to_string(0));
+        }
+
+    }
+    return temp;
+}
+
+
+int main(void){
+    cin >> str;
+    cout << find_max() << '\n';
+    cout << find_min();
+}
