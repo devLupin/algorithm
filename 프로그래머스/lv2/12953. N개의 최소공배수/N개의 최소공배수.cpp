64b06be5f944749cf0c33d5803bcb5ed8743db45
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-10-02 20:41:26 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -9,21 +9,7 @@ int gcd(int a, int b) {
 int lcm(int a, int b) { return (a * b) / gcd(a, b);}
 
 int solution(vector<int> arr) {
-    sort(arr.begin(), arr.end());
-
-    int x = lcm(arr[0], arr[arr.size()-1]);
-    int p = x;
-    
-    while(true) {
-        bool flag = true;
-        for(int i=0; i<arr.size()-1; i++)
-            if(x % arr[i] != 0) {
-                flag = false;
-                break;
-            }
-
-        if(flag) break;
-        x += p;
-    }
-    return x;
+    int ans = arr[0];
+    for(int a : arr) ans = lcm(ans, a);
+    return ans;
 }
