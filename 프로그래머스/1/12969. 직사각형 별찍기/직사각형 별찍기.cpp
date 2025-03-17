b0b9da21011aa34f8b2bb55a47c1cdbd57811058
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2025-03-17 22:13:57 +0900

    [level 1] Title: 직사각형 별찍기, Time: 4.70 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n, m;
+    cin >> n >> m;
+    
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+            cout << "*";
+        cout << '\n';
+    }
+}
